e9d8f555a0201749bc336dd912248b4cecb64d2e
ghrono <ghrono@gmail.com>
2016-01-06 11:52:50 +0300

    Update usbconfig.h

--- usbconfig.h
+++ usbconfig.h
@@ -29,7 +29,7 @@ PDIP/SOIC/TSSOP
        (PCINT5/RESET/ADC0/dW) PB5 □ 1	8 □ VCC
 (PCINT3/XTAL1/CLKI/OC1B/ADC3) PB3 □ 2	7 □ PB2(SCK/USCK/SCL/ADC1/T0/INT0/PCINT2)
 (PCINT4/XTAL2/CLKO/OC1B/ADC2) PB4 □ 3	6 □ PB1(MISO/DO/AIN1/OC0B/OC1A/PCINT1)
-			                           GND □ 4	5 □ PB0(MOSI/DI/SDA/AIN0/OC0A/OCTA/AREF/PCINT0)
+                              GND □ 4	5 □ PB0(MOSI/DI/SDA/AIN0/OC0A/OCTA/AREF/PCINT0)
 			                           
 /* ---------------------------- Hardware Config ---------------------------- */
 
